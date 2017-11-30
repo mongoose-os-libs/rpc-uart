34172b50026edc9e8db182badf0342595caa3aee
Deomid Ryabkov <rojer@cesanta.com>
2017-11-30 22:09:59 +0300

    Remove the concept of "trusted" RPC channel
    Conceived as a temporary access control measure, this is no longer needed now that we have proper authentication and RPC ACL.
    
    PUBLISHED_FROM=0631244c79c289dbb168ee7537388828a8c55348

--- src/mgos_rpc_channel_uart.c
+++ src/mgos_rpc_channel_uart.c
@@ -278,8 +278,7 @@ bool mgos_rpc_uart_init(void) {
   if (mgos_uart_configure(scucfg->uart_no, &ucfg)) {
     struct mg_rpc_channel *uch =
         mg_rpc_channel_uart(scucfg->uart_no, scucfg->wait_for_start_frame);
-    mg_rpc_add_channel(mgos_rpc_get_global(), mg_mk_str(""), uch,
-                       true /* is_trusted */);
+    mg_rpc_add_channel(mgos_rpc_get_global(), mg_mk_str(""), uch);
     uch->ch_connect(uch);
   } else {
     LOG(LL_ERROR, ("UART%d init failed", scucfg->uart_no));
