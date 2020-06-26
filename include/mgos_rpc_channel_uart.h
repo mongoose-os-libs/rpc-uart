3dea04190e0c1d8b6770a2acf26252bbac609017
hrithik098 <hrithiky328@gmail.com>
2020-06-26 11:16:05 +0530

    Added functions in mgos_rpc_channel_uart.h

--- include/mgos_rpc_channel_uart.h
+++ include/mgos_rpc_channel_uart.h
@@ -28,9 +28,25 @@
 extern "C" {
 #endif /* __cplusplus */
 
+// Uart dispatcher
+void mg_rpc_channel_uart_dispatcher(int uart_no, void *arg);
+
+// Uart connect to uart channel
+static void mg_rpc_channel_uart_ch_connect(struct mg_rpc_channel *ch);
+
+// Uart send frame
+static bool mg_rpc_channel_uart_send_frame(struct mg_rpc_channel *ch,const struct mg_str f);
+
+//Uart channel closed
+static void mg_rpc_channel_uart_ch_close(struct mg_rpc_channel *ch);
+
 struct mg_rpc_channel *mg_rpc_channel_uart(
     const struct mgos_config_rpc_uart *cfg,
     const struct mgos_uart_config *ucfg);
+
+// check if config was correct
+bool mgos_rpc_uart_init(void);
+
 
 #ifdef __cplusplus
 }
